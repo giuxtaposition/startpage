f53465a1fc365ea0e531490323e2ff8fb2bc3330
giuxtaposition <yg97.cs@gmail.com>
2022-03-08 13:36:10 +0100

    added link sections

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,39 @@
 <script lang="ts">
+    import Section from './lib/components/Section.svelte'
+
+    const sections = getLinkSectionsData()
+
+    async function getLinkSectionsData() {
+        const apiResponse = await fetch('./links.json')
+
+        return await apiResponse.json()
+    }
 </script>
 
-<main />
+<main>
+    {#await sections then sections}
+        {#each sections as section}
+            <Section
+                title={section.section}
+                links={section.links}
+                color={section.color}
+            />
+        {/each}
+    {/await}
+</main>
 
 <style lang="sass">
 @use 'colors'
+@import url("https://fonts.googleapis.com/css2?family=FiraCode&display=swap")
 
 main
     width: 100vw
     height: 100vh
     background-color: colors.$background-soft-color
-
+    display: flex
+    flex-direction: row
+    justify-content: center
+    align-items: center
+    gap: 1.5rem
+    font-family: Fira Code
 </style>
---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import type { Link } from '../types/Link'
+    export let title: string
+    export let links: Link[]
+    export let color: 'blue' | 'purple' | 'red' | 'orange'
+</script>
+
+<section class={`links-section ${color}`}>
+    <h1>{title}</h1>
+    <ul class="links">
+        {#each links as link}
+            <li class="link">
+                <a href={link.href}>{link.title}</a>
+            </li>
+        {/each}
+    </ul>
+</section>
+
+<style lang="sass">
+@use '../../colors'
+
+.links-section
+    padding: 1rem
+    width: 15rem
+    height: 20rem
+    background-color: colors.$purple-fading-soft
+
+h1
+    margin: 0
+    text-align: center
+    font-size: 1.25rem
+
+.links
+    list-style-type: none
+    display: flex
+    flex-direction: column
+    align-items: center
+    margin: 0.35rem 0 0 0
+    padding: 0
+
+.link
+    padding: 0.05rem
+
+.link a
+    text-decoration: none
+    color: colors.$white-soft
+
+.links-section.blue
+    h1
+        color: colors.$blue
+    .link a:hover
+        color: colors.$blue-soft
+
+.links-section.red
+    h1
+        color: colors.$red
+    .link a:hover
+        color: colors.$red-soft
+
+.links-section.purple
+    h1
+        color: colors.$purple
+    .link a:hover
+        color: colors.$purple-soft
+
+.links-section.orange
+    h1
+        color: colors.$orange
+    .link a:hover
+        color: colors.$orange-soft
+
+</style>
